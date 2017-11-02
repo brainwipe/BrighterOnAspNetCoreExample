985484e41cca987ab69f5790e9988273abfed697
brainwipe <brainwiped@gmail.com>
2017-11-02 16:46:49 +0000

    Fix missing domain project for #1

---
+++ Domain/Commands/CreateValueCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Paramore.Brighter;
+
+namespace Domain.Commands
+{
+    public class CreateValueCommand : IRequest
+    {
+        public CreateValueCommand(string value)
+        {
+            Id = Guid.NewGuid();
+            Value = value;
+        }
+
+        public string Value { get; }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ Domain/Handlers/CreateValueCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using Domain.Commands;
+using Paramore.Brighter;
+
+namespace Domain.Handlers
+{
+    public class CreateValueCommandHandler  : RequestHandler<CreateValueCommand>
+    {
+        public override CreateValueCommand Handle(CreateValueCommand command)
+        {
+            Debug.WriteLine($"Creating value {command.Value}");
+            return base.Handle(command);
+        }
+    }
+}
